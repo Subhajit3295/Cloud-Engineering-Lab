c29de638bb85f163720c21ba25ef55a0035d5b49
Subhajit3295 <clawdrax@gmail.com>
2025-05-16 09:37:04 +0530

    Fix service IDs and update Software Development description in API response

--- ce-lab/app/api/services/route.ts
+++ ce-lab/app/api/services/route.ts
@@ -22,30 +22,36 @@ export async function GET() {
     },
     {
       id: 4,
+      title: "Software Deveopment",
+      description:
+        "Software Development skill encompasses designing, coding, testing, and deploying applications, covering architecture, frontend, backend, app development, optimization, integration, and maintenance.",
+    },
+    {
+      id: 5,
       title: "Cloud",
       description:
         "Cloud programs covers AWS, Azure, and Google Cloud platforms, emphasizing cloud architecture, deployment, and security best practices.",
     },
     {
-      id: 5,
+      id: 6,
       title: "DevOps",
       description:
         "DevOps teaches automation, CI/CD pipelines, and collaboration tools for faster software development and improved deployment processes.",
     },
     {
-      id: 6,
+      id: 7,
       title: "Data Analytics / Data Science",
       description:
         "This skill covers data analysis, visualization, machine learning, and Python tools to extract insights from data.",
     },
     {
-      id: 7,
+      id: 8,
       title: "Internships",
       description:
         "Internships provide hands-on experience in real-world projects, enhancing technical skills and industry exposure for students and freshers.",
     },
     {
-      id: 8,
+      id: 9,
       title: "Capstone Projects",
       description:
         "Capstone projects involve end-to-end development, integrating learned skills to build practical, real-world solutions for diverse industries.",
